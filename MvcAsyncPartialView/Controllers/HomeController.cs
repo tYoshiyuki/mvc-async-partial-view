5989c34f27f6184f91b3386a570d68e818afd581
yTsunomoto <wasiwasimaru@gmail.com>
2019-04-13 01:52:14 +0900

    update

---
+++ MvcAsyncPartialView/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MvcAsyncPartialView.Models;
+
+namespace MvcAsyncPartialView.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Contents()
+        {
+            await Task.Delay(1000);
+            return PartialView("_ContentsPartial");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MvcAsyncPartialView/wwwroot/js/site.js
@@ -0,0 +1,8 @@
+﻿$(function () {
+    $(".partial-contents").each(function (index, item) {
+        var url = $(item).data("action");
+        if (url && url.length > 0) {
+            $(item).load(url);
+        }
+    });
+});
